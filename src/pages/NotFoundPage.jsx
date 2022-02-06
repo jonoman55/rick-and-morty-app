07e5aad8286f94fa1678b6036df501f8b24b5e0f
jonoman55 <jonoman55@yahoo.com>
2022-02-05 20:37:34 -0500

    Update NotFoundPage.jsx
    styled the page

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,16 +1,31 @@
 import { NavLink } from 'react-router-dom';
-import { Box, Typography, Button } from '@mui/material';
+import { Typography, Card, Container, CardMedia } from '@mui/material';
+import { Button } from '../components/controls';
+import rickImg from '../images/pixel-rick.png';
 
-// TODO : Convert into a Paper component
+// TODO : Convert into styled components
 const NotFoundPage = () => (
-    <Box sx={{ display: 'flex', flexDirection: 'column', flexWrap: 'nowrap', justifyContent: 'center', alignItems: 'center' }}>
-        <Typography component='p' variant='body1' sx={{ mb: 2 }}>
-            404 - Page Not Found
-        </Typography>
-        <Button variant='contained' LinkComponent={NavLink} to='/'>
-            Home
-        </Button>
-    </Box>
+    <Container maxWidth='xs'>
+        <Card elevation={2} sx={{
+            p: 2, width: '100%', display: 'flex', flexDirection: 'column', flexWrap: 'nowrap', justifyContent: 'center', alignItems: 'center'
+        }}>
+            <Typography component='h4' variant='h5' sx={{ my: 2, color: 'primary.contrastText', fontWeight: 'bold' }}>
+                404 - Page Not Found
+            </Typography>
+            <CardMedia
+                component='img'
+                src={rickImg}
+                alt=''
+                sx={{
+                    border: '1px solid', borderRadius: (theme) => theme.shape.borderRadius, borderColor: 'custom.black',
+                    bgcolor: (theme) => theme.palette.mode === 'light' ? 'custom.disabled' : 'custom.white', opacity: 0.9
+                }}
+            />
+            <Button variant='contained' LinkComponent={NavLink} to='/' sx={{ my: 2 }}>
+                Home
+            </Button>
+        </Card>
+    </Container>
 );
 
 export default NotFoundPage;
